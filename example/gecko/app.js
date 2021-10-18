eb647e19b8aa55c8287d5b4a8f2727905e65df65
sosuke-k <snoopies.drum@gmail.com>
2021-10-18 23:40:11 +0900

    add gecko plugin and example

--- example/gecko/app.js
+++ example/gecko/app.js
@@ -1,37 +1,33 @@
-'use strict';
+"use strict";
 
 // Create an instance
 var wavesurfer;
 
 // Init & load
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     let options = {
-        container: '#waveform',
-        waveColor: 'violet',
-        progressColor: 'purple',
-        loaderColor: 'purple',
-        cursorColor: 'navy',
-        selectionColor: '#d0e9c6',
+        container: "#waveform",
+        waveColor: "violet",
+        progressColor: "purple",
+        loaderColor: "purple",
+        cursorColor: "navy",
+        selectionColor: "#d0e9c6",
         loopSelection: false,
         plugins: [
-            WaveSurfer.elan.create({
-                url: 'transcripts/001z.xml',
-                container: '#annotations',
-                tiers: {
-                    Text: true,
-                    Comments: true
-                }
+            WaveSurfer.gecko.create({
+                url: "transcripts/OGVC_dlg.json",
+                container: "#annotations",
             }),
-            WaveSurfer.regions.create()
-        ]
+            WaveSurfer.regions.create(),
+        ],
     };
 
-    if (location.search.match('scroll')) {
+    if (location.search.match("scroll")) {
         options.minPxPerSec = 100;
         options.scrollParent = true;
     }
 
-    if (location.search.match('normalize')) {
+    if (location.search.match("normalize")) {
         options.normalize = true;
     }
 
@@ -39,44 +35,44 @@ document.addEventListener('DOMContentLoaded', function() {
     wavesurfer = WaveSurfer.create(options);
 
     /* Progress bar */
-    (function() {
-        let progressDiv = document.querySelector('#progress-bar');
-        let progressBar = progressDiv.querySelector('.progress-bar');
+    (function () {
+        let progressDiv = document.querySelector("#progress-bar");
+        let progressBar = progressDiv.querySelector(".progress-bar");
 
-        let showProgress = function(percent) {
-            progressDiv.style.display = 'block';
-            progressBar.style.width = percent + '%';
+        let showProgress = function (percent) {
+            progressDiv.style.display = "block";
+            progressBar.style.width = percent + "%";
         };
 
-        let hideProgress = function() {
-            progressDiv.style.display = 'none';
+        let hideProgress = function () {
+            progressDiv.style.display = "none";
         };
 
-        wavesurfer.on('loading', showProgress);
-        wavesurfer.on('ready', hideProgress);
-        wavesurfer.on('destroy', hideProgress);
-        wavesurfer.on('error', hideProgress);
+        wavesurfer.on("loading", showProgress);
+        wavesurfer.on("ready", hideProgress);
+        wavesurfer.on("destroy", hideProgress);
+        wavesurfer.on("error", hideProgress);
     })();
 
-    wavesurfer.elan.on('ready', function(data) {
-        wavesurfer.load('transcripts/' + data.media.url);
+    wavesurfer.gecko.on("ready", function (data) {
+        wavesurfer.load("transcripts/OGVC_dlg.wav");
     });
 
-    wavesurfer.elan.on('select', function(start, end) {
+    wavesurfer.gecko.on("select", function (start, end) {
         wavesurfer.backend.play(start, end);
     });
 
-    wavesurfer.elan.on('ready', function() {
-        let classList = wavesurfer.elan.container.querySelector('table')
-            .classList;
-        ['table', 'table-striped', 'table-hover'].forEach(function(cl) {
+    wavesurfer.gecko.on("ready", function () {
+        let classList =
+            wavesurfer.gecko.container.querySelector("table").classList;
+        ["table", "table-striped", "table-hover"].forEach(function (cl) {
             classList.add(cl);
         });
     });
 
     let prevAnnotation, prevRow, region;
-    let onProgress = function(time) {
-        let annotation = wavesurfer.elan.getRenderedAnnotation(time);
+    let onProgress = function (time) {
+        let annotation = wavesurfer.gecko.getRenderedAnnotation(time);
 
         if (prevAnnotation != annotation) {
             prevAnnotation = annotation;
@@ -86,13 +82,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
             if (annotation) {
                 // Highlight annotation table row
-                let row = wavesurfer.elan.getAnnotationNode(annotation);
-                prevRow && prevRow.classList.remove('success');
+                let row = wavesurfer.gecko.getAnnotationNode(annotation);
+                prevRow && prevRow.classList.remove("success");
                 prevRow = row;
-                row.classList.add('success');
+                row.classList.add("success");
                 let before = row.previousSibling;
                 if (before) {
-                    wavesurfer.elan.container.scrollTop = before.offsetTop;
+                    wavesurfer.gecko.container.scrollTop = before.offsetTop;
                 }
 
                 // Region
@@ -100,15 +96,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     start: annotation.start,
                     end: annotation.end,
                     resize: false,
-                    color: 'rgba(223, 240, 216, 0.7)'
+                    color: "rgba(223, 240, 216, 0.7)",
                 });
             }
         }
     };
 
-    wavesurfer.on('audioprocess', onProgress);
+    wavesurfer.on("audioprocess", onProgress);
 
-    wavesurfer.on('error', function(e) {
+    wavesurfer.on("error", function (e) {
         console.warn(e);
     });
 });
--- src/plugin/gecko/index.js
+++ src/plugin/gecko/index.js
@@ -1,297 +1,166 @@
 /**
- * @typedef {Object} ElanPluginParams
+ * @typedef {Object} GeckoPluginParams
  * @property {string|HTMLElement} container CSS selector or HTML element where
- * the ELAN information should be rendered.
- * @property {string} url The location of ELAN XML data
+ * the Gecko information should be rendered.
+ * @property {string} url The location of Gecko JSON data
  * @property {?boolean} deferInit Set to true to manually call
- * @property {?Object} tiers If set only shows the data tiers with the `TIER_ID`
- * in this map.
+ * @property {?Object} monologues If set only shows the data monologues in this map.
  */
 
 /**
- * Downloads and renders ELAN audio transcription documents alongside the
+ * Downloads and renders Gecko JSON audio transcription documents alongside the
  * waveform.
  *
  * @implements {PluginClass}
  * @extends {Observer}
  * @example
- * // es6
- * import ElanPlugin from 'wavesurfer.elan.js';
- *
- * // commonjs
- * var ElanPlugin = require('wavesurfer.elan.js');
- *
  * // if you are using <script> tags
- * var ElanPlugin = window.WaveSurfer.elan;
+ * var GeckoPlugin = window.WaveSurfer.gecko;
  *
  * // ... initialising wavesurfer with the plugin
  * var wavesurfer = WaveSurfer.create({
  *   // wavesurfer options ...
  *   plugins: [
- *     ElanPlugin.create({
+ *     GeckoPlugin.create({
  *       // plugin options ...
  *     })
  *   ]
  * });
  */
-export default class ElanPlugin {
+export default class GeckoPlugin {
     /**
-     * Elan plugin definition factory
+     * Gecko plugin definition factory
      *
      * This function must be used to create a plugin definition which can be
      * used by wavesurfer to correctly instantiate the plugin.
      *
-     * @param  {ElanPluginParams} params parameters use to initialise the plugin
+     * @param  {GeckoPluginParams} params parameters use to initialise the plugin
      * @return {PluginDefinition} an object representing the plugin
      */
     static create(params) {
         return {
-            name: 'elan',
+            name: "gecko",
             deferInit: params && params.deferInit ? params.deferInit : false,
             params: params,
-            instance: ElanPlugin
+            instance: GeckoPlugin,
         };
     }
-
-    Types = {
-        ALIGNABLE_ANNOTATION: 'ALIGNABLE_ANNOTATION',
-        REF_ANNOTATION: 'REF_ANNOTATION'
-    };
 
     constructor(params, ws) {
         this.data = null;
         this.params = params;
         this.container =
-            'string' == typeof params.container
+            "string" == typeof params.container
                 ? document.querySelector(params.container)
                 : params.container;
 
         if (!this.container) {
-            throw Error('No container for ELAN');
+            throw Error("No container for Gecko");
         }
     }
 
     init() {
-        this.bindClick();
+        const _map = Array.prototype.map;
+
+        this.data = {
+            media: {},
+            annotations: [],
+        };
 
         if (this.params.url) {
-            this.load(this.params.url);
+            fetch(this.params.url).then((data) => {
+                data.json().then((jsondata) => {
+                    // TODO: Check keys
+                    const monologues = jsondata["monologues"];
+                    const annotations = _map.call(monologues, (mono, index) => {
+                        const anno = {
+                            id: index,
+                            start: mono.start,
+                            end: mono.end,
+                            text: mono.terms[0].text,
+                        };
+                        return anno;
+                    });
+                    this.render(annotations);
+                    this.data.annotations = annotations;
+                    this.bindClick();
+                    this.fireEvent("ready", this.data);
+                });
+            });
         }
     }
 
     destroy() {
-        this.container.removeEventListener('click', this._onClick);
+        this.container.removeEventListener("click", this._onClick);
         this.container.removeChild(this.table);
     }
 
-    load(url) {
-        this.loadXML(url, xml => {
-            this.data = this.parseElan(xml);
-            this.render();
-            this.fireEvent('ready', this.data);
-        });
-    }
-
-    loadXML(url, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'document';
-        xhr.send();
-        xhr.addEventListener('load', e => {
-            callback && callback(e.target.responseXML);
-        });
-    }
-
-    parseElan(xml) {
-        const _forEach = Array.prototype.forEach;
-        const _map = Array.prototype.map;
-
-        const data = {
-            media: {},
-            timeOrder: {},
-            tiers: [],
-            annotations: {},
-            alignableAnnotations: []
-        };
-
-        const header = xml.querySelector('HEADER');
-        const inMilliseconds =
-            header.getAttribute('TIME_UNITS') == 'milliseconds';
-        const media = header.querySelector('MEDIA_DESCRIPTOR');
-        data.media.url = media.getAttribute('MEDIA_URL');
-        data.media.type = media.getAttribute('MIME_TYPE');
-
-        const timeSlots = xml.querySelectorAll('TIME_ORDER TIME_SLOT');
-        const timeOrder = {};
-        _forEach.call(timeSlots, slot => {
-            let value = parseFloat(slot.getAttribute('TIME_VALUE'));
-            // If in milliseconds, convert to seconds with rounding
-            if (inMilliseconds) {
-                value = Math.round(value * 1e2) / 1e5;
-            }
-            timeOrder[slot.getAttribute('TIME_SLOT_ID')] = value;
-        });
-
-        data.tiers = _map.call(xml.querySelectorAll('TIER'), tier => ({
-            id: tier.getAttribute('TIER_ID'),
-            linguisticTypeRef: tier.getAttribute('LINGUISTIC_TYPE_REF'),
-            defaultLocale: tier.getAttribute('DEFAULT_LOCALE'),
-            annotations: _map.call(
-                tier.querySelectorAll('REF_ANNOTATION, ALIGNABLE_ANNOTATION'),
-                node => {
-                    const annot = {
-                        type: node.nodeName,
-                        id: node.getAttribute('ANNOTATION_ID'),
-                        ref: node.getAttribute('ANNOTATION_REF'),
-                        value: node
-                            .querySelector('ANNOTATION_VALUE')
-                            .textContent.trim()
-                    };
-
-                    if (this.Types.ALIGNABLE_ANNOTATION == annot.type) {
-                        // Add start & end to alignable annotation
-                        annot.start =
-                            timeOrder[node.getAttribute('TIME_SLOT_REF1')];
-                        annot.end =
-                            timeOrder[node.getAttribute('TIME_SLOT_REF2')];
-                        // Add to the list of alignable annotations
-                        data.alignableAnnotations.push(annot);
-                    }
-
-                    // Additionally, put into the flat map of all annotations
-                    data.annotations[annot.id] = annot;
-
-                    return annot;
-                }
-            )
-        }));
-
-        // Create JavaScript references between annotations
-        data.tiers.forEach(tier => {
-            tier.annotations.forEach(annot => {
-                if (null != annot.ref) {
-                    annot.reference = data.annotations[annot.ref];
-                }
-            });
-        });
-
-        // Sort alignable annotations by start & end
-        data.alignableAnnotations.sort((a, b) => {
-            let d = a.start - b.start;
-            if (d == 0) {
-                d = b.end - a.end;
-            }
-            return d;
-        });
-
-        data.length = data.alignableAnnotations.length;
-
-        return data;
-    }
-
-    render() {
-        // apply tiers filter
-        let tiers = this.data.tiers;
-        if (this.params.tiers) {
-            tiers = tiers.filter(tier => tier.id in this.params.tiers);
-        }
-
-        // denormalize references to alignable annotations
-        const backRefs = {};
-        let indeces = {};
-        tiers.forEach((tier, index) => {
-            tier.annotations.forEach(annot => {
-                if (
-                    annot.reference &&
-                    annot.reference.type == this.Types.ALIGNABLE_ANNOTATION
-                ) {
-                    if (!(annot.reference.id in backRefs)) {
-                        backRefs[annot.ref] = {};
-                    }
-                    backRefs[annot.ref][index] = annot;
-                    indeces[index] = true;
-                }
-            });
-        });
-        indeces = Object.keys(indeces).sort();
-
-        this.renderedAlignable = this.data.alignableAnnotations.filter(
-            alignable => backRefs[alignable.id]
-        );
-
+    render(annotations) {
         // table
-        const table = (this.table = document.createElement('table'));
-        table.className = 'wavesurfer-annotations';
+        const table = (this.table = document.createElement("table"));
+        table.className = "wavesurfer-annotations";
 
         // head
-        const thead = document.createElement('thead');
-        const headRow = document.createElement('tr');
+        const thead = document.createElement("thead");
+        const headRow = document.createElement("tr");
         thead.appendChild(headRow);
         table.appendChild(thead);
-        const th = document.createElement('th');
-        th.textContent = 'Time';
-        th.className = 'wavesurfer-time';
-        headRow.appendChild(th);
-        indeces.forEach(index => {
-            const tier = tiers[index];
-            const th = document.createElement('th');
-            th.className = 'wavesurfer-tier-' + tier.id;
-            th.textContent = tier.id;
-            if (this.params.tiers) { th.style.width = this.params.tiers[tier.id]; }
-            headRow.appendChild(th);
-        });
+        const timeTh = document.createElement("th");
+        timeTh.textContent = "Time";
+        timeTh.className = "wavesurfer-time";
+        headRow.appendChild(timeTh);
+
+        const textTh = document.createElement("th");
+        textTh.className = "wavesurfer-tier-";
+        textTh.textContent = "Text";
+        headRow.appendChild(textTh);
 
         // body
-        const tbody = document.createElement('tbody');
+        const tbody = document.createElement("tbody");
         table.appendChild(tbody);
-        this.renderedAlignable.forEach(alignable => {
-            const row = document.createElement('tr');
-            row.id = 'wavesurfer-alignable-' + alignable.id;
+        annotations.forEach((anno) => {
+            const row = document.createElement("tr");
+            row.id = "wavesurfer-annotation-" + anno.id;
             tbody.appendChild(row);
 
-            const td = document.createElement('td');
-            td.className = 'wavesurfer-time';
-            td.textContent =
-                alignable.start.toFixed(1) + '–' + alignable.end.toFixed(1);
-            row.appendChild(td);
+            const timeTd = document.createElement("td");
+            timeTd.className = "wavesurfer-time";
+            timeTd.textContent =
+                anno.start.toFixed(1) + "–" + anno.end.toFixed(1);
+            row.appendChild(timeTd);
 
-            const backRef = backRefs[alignable.id];
-            indeces.forEach(index => {
-                const tier = tiers[index];
-                const td = document.createElement('td');
-                const annotation = backRef[index];
-                if (annotation) {
-                    td.id = 'wavesurfer-annotation-' + annotation.id;
-                    td.dataset.ref = alignable.id;
-                    td.dataset.start = alignable.start;
-                    td.dataset.end = alignable.end;
-                    td.textContent = annotation.value;
-                }
-                td.className = 'wavesurfer-tier-' + tier.id;
-                row.appendChild(td);
-            });
+            const textTd = document.createElement("td");
+            textTd.id = "wavesurfer-text-" + anno.id;
+            textTd.dataset.ref = anno.id;
+            textTd.dataset.start = anno.start;
+            textTd.dataset.end = anno.end;
+            textTd.textContent = anno.text;
+            textTd.className = "wavesurfer-tier-";
+            row.appendChild(textTd);
         });
 
-        this.container.innerHTML = '';
+        // add
+        this.container.innerHTML = "";
         this.container.appendChild(table);
     }
 
     bindClick() {
-        this._onClick = e => {
+        this._onClick = (e) => {
             const ref = e.target.dataset.ref;
             if (null != ref) {
-                const annot = this.data.annotations[ref];
+                const index = parseInt(ref);
+                const annot = this.data.annotations[index];
                 if (annot) {
-                    this.fireEvent('select', annot.start, annot.end);
+                    this.fireEvent("select", annot.start, annot.end);
                 }
             }
         };
-        this.container.addEventListener('click', this._onClick);
+        this.container.addEventListener("click", this._onClick);
     }
 
     getRenderedAnnotation(time) {
         let result;
-        this.renderedAlignable.some(annotation => {
+        this.data.annotations.some((annotation) => {
             if (annotation.start <= time && annotation.end >= time) {
                 result = annotation;
                 return true;
@@ -302,6 +171,8 @@ export default class ElanPlugin {
     }
 
     getAnnotationNode(annotation) {
-        return document.getElementById('wavesurfer-alignable-' + annotation.id);
+        return document.getElementById(
+            "wavesurfer-annotation-" + annotation.id
+        );
     }
 }
